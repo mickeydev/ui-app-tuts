8ea3259c01224d1270d5f81e16440cdeceb9f929
Michael Yeboah Frimpong <46923296+mickeydev@users.noreply.github.com>
2022-09-03 13:27:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,22 +11,26 @@ import {
    Toolbar,
    Container,
    Button,
-   ButtonGroup,
 } from "@material-ui/core";
 import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./style";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = (props) => {
+   const classes = useStyles();
+
    return (
       <>
          <CssBaseline />
          <AppBar position="relative">
             <Toolbar>
-               <PhotoCamera />
+               <PhotoCamera className={classes.icon} />
                <Typography variant="h6">Photo Album</Typography>
             </Toolbar>
          </AppBar>
          <main>
-            <div>
+            <div className={classes.container}>
                <Container maxWidth="sm">
                   <Typography
                      variant="h2"
@@ -42,18 +46,63 @@ const App = (props) => {
                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
                   </Typography>
+                  <div className={classes.buttons}>
+                     <Grid container spacing={2} justifyContent="center">
+                        <Grid item>
+                           <Button variant="contained" color="primary">
+                              See my photos
+                           </Button>
+                        </Grid>
+                        <Grid item>
+                           <Button variant="outlined" color="primary">
+                              Secondary Action
+                           </Button>
+                        </Grid>
+                     </Grid>
+                  </div>
                </Container>
             </div>
-            <ButtonGroup
-               variant="contained"
-               aria-label="outlined primary button group"
-               color="primary"
-            >
-               <Button>One</Button>
-               <Button>Two</Button>
-               <Button>Three</Button>
-            </ButtonGroup>
+            <Container className={classes.cardGrid} maxWidth="md">
+               <Grid container spacing={4}>
+                  {cards.map((card) => (
+                     <Grid item key={card} xs={12} sm={6} md={4}>
+                        <Card className={classes.card}>
+                           <CardMedia
+                              className={classes.cardMedia}
+                              image="https://source.unsplash.com/random"
+                              title="Image Title"
+                           />
+                           <CardContent className={classes.cardContent}>
+                              <Typography gutterBottom variant="h5">
+                                 Heading
+                              </Typography>
+                              <Typography>
+                                 This is where the card content goes. And that
+                                 is the card content.
+                              </Typography>
+                           </CardContent>
+                           <CardActions>
+                              <Button size="small" color="primary">
+                                 View
+                              </Button>
+                              <Button size="small" color="primary">
+                                 Edit
+                              </Button>
+                           </CardActions>
+                        </Card>
+                     </Grid>
+                  ))}
+               </Grid>
+            </Container>
          </main>
+         <footer className={classes.footer}>
+            <Typography variant="h6" align="center" gutterBottom>
+               Footer
+            </Typography>
+            <Typography variant="subtitle1" align="center">
+               Something to give the Footer a nice looking style.
+            </Typography>
+         </footer>
       </>
    );
 };
